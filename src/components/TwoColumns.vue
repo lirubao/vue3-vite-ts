cd74f663add85155307a71de4d1656529d7a63e1
lirubao <lirubaogo@gmail.com>
2021-01-06 04:21:33 +0800

    两栏设计

--- src/components/TwoColumns.vue
+++ src/components/TwoColumns.vue
@@ -8,7 +8,7 @@
 <script lang="ts">
   import { defineComponent } from 'vue'
   export default defineComponent({
-    name: 'two_columns',
+    name: 'TwoColumns',
     components: {},
   })
 </script>
